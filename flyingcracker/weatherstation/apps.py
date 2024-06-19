9ba0c40953861a3462711499abff760cbde9aeab
Graham Ullrich <graham@flyingcracker.com>
2024-06-19 16:26:22 -0600

    include default_auto_field in apps.py

--- flyingcracker/weatherstation/apps.py
+++ flyingcracker/weatherstation/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class WeatherStationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "weatherstation"
     verbose_name = "WeatherStation app"
